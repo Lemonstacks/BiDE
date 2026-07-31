02553078a37e7fb4a95033482e2e49d07bb7ac88
Lemonstacks <lemonstacks@gmail.com>
2026-07-31 10:50:42 +0200

    feat: add deployment prep (Dockerfile, production config, auto-migrations)

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -22,6 +22,13 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
+// Auto-apply pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
